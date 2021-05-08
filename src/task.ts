2718bc1c67d0437f6d82e197b732755ec391c8fd
aleksandr <aleksandr.leontev.n@mail.ru>
2021-05-08 18:46:42 +0300

    initial commit

--- src/task.ts
+++ src/task.ts
@@ -1,15 +1,4 @@
-/**
- Задание к TypeScript: Part 2 будет проверять ваше умение использовать и вычислять типы
- Все задания устроены таким образом что в них есть тип FIXME (который any) и ваша задача избавится от него
- Менять код кроме типов нельзя, исходные типы менять тоже нельзя, но можно рефакторить
- Например `type A = 1 | 2` выразить как `type A1 = 1; type A2 = 2; type A = A1 | A2;`
- **/
-
-// В функцию приходит массив состояний заказа и фильтруется
-// Нужно заменить FIXME на тип который вычисляется на освове OrderState
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
 
 const orderStates = [
     "initial",
@@ -17,25 +6,21 @@ const orderStates = [
     "buyingSupplies",
     "producing",
     "fullfilled",
+    "ass"
 ] as const;
 
 type OrderState = typeof orderStates[number];
 
-export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
-    const filteredStates = [] as FIXME;
+const getUserOrderStates = (orderStates: OrderState[]): OrderState[] => {
+    const filteredStates = [] as OrderState[];
     orderStates.forEach((element) => {
-        if (element !== "buyingSupplies" && element !== "producing") {
+        if (element !== "ass" && element !== "producing") {
             filteredStates.push(element);
         }
     });
     return filteredStates;
 };
 
-// Есть объединение (юнион) типов заказов в различных состояниях
-// Нужно заменить FIXME на тип который достанет из Order все возможные состояния (state)
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
 
 type Order =
     | {
@@ -69,117 +54,40 @@ type Order =
     fullfillmentDate: Date;
 };
 
-export const getOrderState = (order: Order): FIXME => order.state;
+const getOrderState = (order: Order): OrderState => order.state;
 
+const getSum = (order: Order): number => order.sum;
 
-// Есть общая функция omit которая удаляет поле из объекта и возвращает его без этого поля
-// Нужно заменить FIXME на соответствующий тип
+console.log(getOrderState({
+    state: "inWork",
+    sum: 1,
+    workerId: 1
+}));
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+console.log(getSum({
+    state: "inWork",
+    sum: 1,
+    workerId: 1
+}));
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const omit = <T extends Record<any, any>, K extends keyof T>(
+const omit = <T extends Record<any, any>, K extends keyof T>(
     obj: T,
     keyToOmit: K
-): FIXME => {
+): Record<any, any> => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { [keyToOmit]: _, ...withoutKey } = obj;
     return withoutKey;
 };
 
-// Есть объединение (юнион) типов заказов в различных состояниях
-// и функция filterOnlyInitialAndInWorkOrder которая принимает заказы в любых состояниях
-// А возвращает только initial и inWork
-// Нужно заменить FIXME на правильный тип вычисленный на основе Order
+interface ObjectRecord extends Record<number, number> {
+    1: number,
+    2: number
+}
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
-
-type Order =
-    | {
-    state: "initial";
-    sum: number;
-}
-    | {
-    state: "inWork";
-    sum: number;
-    workerId: number;
-}
-    | {
-    state: "buyingSupplies";
-    sum: number;
-    workerId: number;
-    suppliesSum: number;
-}
-    | {
-    state: "producing";
-    sum: number;
-    workerId: number;
-    suppliesSum: number;
-    produceEstimate: Date;
-}
-    | {
-    state: "fullfilled";
-    sum: number;
-    workerId: number;
-    suppliesSum: number;
-    produceEstimate: Date;
-    fullfillmentDate: Date;
+const objectRecord: ObjectRecord = {
+    1: 1,
+    2: 2
 };
 
-export const filterOnlyInitialAndInWorkOrder = (order: Order): FIXME => {
-    if (order.state === "initial" || order.state === "inWork") {
-        return order;
-    }
-
-    return null;
-};
-
-// Есть функция которая достает из реакт компонента (любого, и Functional и Class) его defaultProps
-// Нужно заменить FIXME на правильный тип
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
-
-// Hint: infer
-export const getDefaultProps = <T>(
-    component: React.ComponentType<T>
-): FIXME => {
-    return component.defaultProps;
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
-
-const orderStates = [
-    "initial",
-    "inWork",
-    "buyingSupplies",
-    "producing",
-    "fullfilled",
-] as const;
-
-type OrderState = typeof orderStates[number];
-
-// Hint: type guards
-export const getUserOrderStates = (orderStates: OrderState[]): FIXME =>
-    orderStates.filter(
-        (state) => state !== "buyingSupplies" && state !== "producing"
-    );
-
-// Задача состоит в том что написать калькулято для натуральных чисел, но только на типах!
-// Ниже приведена заготовка
-// Хочется поддержки сложения и вычитания, если хочется еще челленджа, то деление и умножение
-// Из-за ограничений глубины вычислений поддержать все натуральные числа не получится, это нормально
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
-
-type Equals<A, B> = A extends B ? (B extends A ? "success" : never) : never;
-
-type Add<A, B> = FIXME;
-type Subtract<A, B> = FIXME;
-
-export type OnePlusOneTest = Equals<Add<1, 1>, 2>;
-export type TwoMinusOneTest = Equals<Subtract<2, 1>, 1>;
+console.log(objectRecord);
+console.log(omit(objectRecord, 1));
