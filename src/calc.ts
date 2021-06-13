f7173493739008270acd00c5c33587fcacae2f3e
aleksandr <aleksandr.leontev.n@mail.ru>
2021-06-13 19:06:09 +0300

    add calc

--- src/calc.ts
+++ src/calc.ts
@@ -1,9 +1,21 @@
-type FIXME = any;
+type Equals<A, B> = A extends B ? (B extends A ? 'success' : never) : never;
 
-type Equals<A, B> = A extends B ? (B extends A ? "success" : never) : never;
+type BuildTuple<L extends number, T extends any[] = []> = T extends {length: L;}
+    ? T
+    : BuildTuple<L, [...T, any]>;
+//у массивов есть Length и поэтому мы используем infer
+type Length<T extends any[]> = T extends { length: infer L } ? L : never;
+// строим два кортежа
+type Add<A extends number, B extends number> = Length<[...BuildTuple<A>, ...BuildTuple<B>]>;
 
-type Add<A, B> = FIXME;
-type Subtract<A, B> = FIXME;
+// берем  длину остаточных параметров от кортежа длиной А объединенного с кортежем B
+// создавая большой кортеж длины A
+type Subtract<A extends number, B extends number> = BuildTuple<A> extends [
+        ...infer U,
+        ...BuildTuple<B>
+    ]
+    ? Length<U>
+    : never;
 
-export type OnePlusOneTest = Equals<Add<1, 1>, 2>;
-export type TwoMinusOneTest = Equals<Subtract<2, 1>, 1>;
+export type Plus = Equals<Add<1, 1>, 2>;
+export type Minus = Equals<Subtract<2, 1>, 1>;
--- src/filterOnlyInitialAndInWorkOrder.ts
+++ src/filterOnlyInitialAndInWorkOrder.ts
@@ -1,10 +1,3 @@
-// Есть объединение (юнион) типов заказов в различных состояниях
-// и функция filterOnlyInitialAndInWorkOrder которая принимает заказы в любых состояниях
-// А возвращает только initial и inWork
-// Нужно заменить FIXME на правильный тип вычисленный на основе Order
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-
 export type Order =
   | {
       state: "initial";
@@ -52,4 +45,4 @@ const work: Order = {
   sum: 1,
   workerId: 22,
 };
-// filterOnlyInitialAndInWorkOrder(work);
+
